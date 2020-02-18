360fe9f97a10d7d2da9f3d7aa723d088a4424e91
Johannes Wenzel <johannes91@gmail.com>
2020-02-18 16:09:34 +0100

    keyboard avoid example

--- step05-KeyboardAvoidingView/App.tsx
+++ step05-KeyboardAvoidingView/App.tsx
@@ -1,19 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  KeyboardAvoidingView,
+  TextInput,
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard
+} from "react-native";
 
 export default function App() {
+  const [textInputValue, setTextInputValue] = React.useState("TextInput1");
+  const [textInputValue2, setTextInputValue2] = React.useState("TextInput2");
+  const [textInputValue3, setTextInputValue3] = React.useState("TextInput3");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => Keyboard.dismiss()}
+      style={{ flex: 1 }}
+    >
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="padding"
+        keyboardVerticalOffset={30} // Distance between TextInput & Keyboard
+      >
+        <TextInput
+          value={textInputValue}
+          style={{
+            ...styles.textInput
+          }}
+          onChangeText={e => setTextInputValue(e)}
+        />
+        <View style={{ width: "100%", alignItems: "center" }}>
+          <TextInput
+            value={textInputValue2}
+            style={{
+              ...styles.textInput,
+              marginBottom: 20
+            }}
+            onChangeText={e => setTextInputValue2(e)}
+          />
+          <TextInput
+            value={textInputValue3}
+            style={styles.textInput}
+            onChangeText={e => setTextInputValue3(e)}
+          />
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 80,
+    marginBottom: 50,
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
+  textInput: {
+    borderWidth: 1,
+    borderRadius: 4,
+    width: "90%",
+    padding: 5
+  }
 });
