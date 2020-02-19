cfc7ab541f3c20e4d52f385912dab4444acaee7c
Sven <sven@efec.de>
2020-02-19 11:35:17 +0100

    customHooks

--- step07-Hooks/App.tsx
+++ step07-Hooks/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
 
+
+function usePeople() {
+  const [people, setPeople] = React.useState([]);
+  
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        const fetchP = await fetch("https://swapi.co/api/people/");
+        const res = await fetchP.json();
+        console.log('res', res)
+        setPeople(res.results);
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
+    getData();
+  }, );
+  return people;
+}
 export default function App() {
 
   const [count, setCount] = React.useState(0);
   const [reload, setReload] = React.useState(true);
   const [title, setTitle] = React.useState('');
   const [people, setPeople] = React.useState([]);
+  // const people = usePeople();
 
   React.useEffect(() => {
     // By default, React runs the effects after every render 
@@ -14,37 +34,37 @@ export default function App() {
     setTitle(`You clicked ${count} times`);
   });
 
-  // React.useEffect(() => {
-  //   const getData = async () => {
-  //     try {
-  //       setCount(count + 1);
-  //       const fetchP = await fetch("https://swapi.co/api/people/");
-  //       const res = await fetchP.json();
-  //       console.log('res', res)
-  //       setPeople(res.results);
-  //       setReload(false);
-  //     } catch (error) {
-  //       console.log('error', error);
-  //     }
-  //   };
-  //   getData();
-  // }, [reload]);
-
   React.useEffect(() => {
     const getData = async () => {
-      setCount(count + 1)
-      const res = await fetch("https://swapi.co/api/people/");
-      res
-        .json()
-        .then(res => {
-          console.log('res', res);
-          setPeople(res.results);
-          setReload(false);
-        })
-        .catch(err => console.log(err));
+      try {
+        setCount(count + 1);
+        const fetchP = await fetch("https://swapi.co/api/people/");
+        const res = await fetchP.json();
+        console.log('res', res)
+        setPeople(res.results);
+        setReload(false);
+      } catch (error) {
+        console.log('error', error);
+      }
     };
     getData();
   }, [reload]);
+
+  // React.useEffect(() => {
+  //   const getData = async () => {
+  //     setCount(count + 1)
+  //     const res = await fetch("https://swapi.co/api/people/");
+  //     res
+  //       .json()
+  //       .then(res => {
+  //         console.log('res', res);
+  //         setPeople(res.results);
+  //         setReload(false);
+  //       })
+  //       .catch(err => console.log(err));
+  //   };
+  //   getData();
+  // }, [reload]);
 
 
   // Rules of Hooks
